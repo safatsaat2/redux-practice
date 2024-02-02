80bda018f35b90f4672d27e6c58e2b4f3a6be8ab
Sakib Rahman Safat <sakibsafat47@gmail.com>
2024-02-02 21:10:16 +0600

    slice create

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+            <h2 className='text-xl text-blue-900 bg-slate-400'>Hello</h2>
         </div>
     );
 };
