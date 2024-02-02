3072fdf1d89aceb84133419630cbe6a128630611
Sakib Rahman Safat <sakibsafat47@gmail.com>
2024-02-03 00:14:13 +0600

    counter state management complete

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from '../redux/features/counter/counterSlice';
 
 const Counter = () => {
-    const {count} = useSelector((state) => state.counter)
+    const {count} = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
     return (
         <div>
-          <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Increment</button>
+          <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' onClick={() => dispatch(increment())}>Increment</button>
           <div>
-            <h1>{count}</h1>
+            <h1>{
+                count <= 0 ? "less than 0" : count
+                }</h1>
           </div>
-          <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Decrement</button>  
+          <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' onClick={() => dispatch(decrement())}>Decrement</button>  
         </div>
     );
 };
